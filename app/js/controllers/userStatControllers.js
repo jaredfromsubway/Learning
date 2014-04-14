381f35f3855be74fb0e4c3e31e027dab5653002f
JaredFromSubway <theduderino64@yahoo.com>
2014-04-13 20:11:29 -0700

    userstatelist and bootstrap grid refinement

--- app/js/controllers/userStatControllers.js
+++ app/js/controllers/userStatControllers.js
@@ -2,12 +2,32 @@
 
 angular.module('coffeeTimeControllers', [])
     .controller('userStatListController', ['$scope','$stateParams', 'userStatList', function($scope, $stateParams, userStatList){
-        angular.forEach(userStatList, function (value, key) {
+        this.getHealthIcons = function(iRelativeHealth){
+            var aIcons = [];
+            var iStep = 10;
+            for (var i = iStep; i <= 100; i = i + iStep){
+                var oIcon = {};
+                oIcon.hasHealth = iRelativeHealth > i - iStep ? true : false;
+                oIcon.opacity = 1;
+                if (oIcon.hasHealth && iRelativeHealth < i){
+                    console.log(iRelativeHealth);
+                    console.log(i);
+                    oIcon.opacity = (iStep - (i % iRelativeHealth)) / iStep;
+                }
+                aIcons.push(oIcon);
+            }
+            return aIcons;
+        }
 
+        angular.forEach(userStatList, function (value, key) {
             userStatList[key].fullName = userStatList[key].firstName + ' ' + userStatList[key].lastName;
-        });
+            userStatList[key].healthIconsMeta = this.getHealthIcons(userStatList[key].health);
+        }, this);
         $scope.userStatItems = userStatList;
     }])
+
+
+
     .controller('userStatItemController', ['$scope','$stateParams', 'userStatItem', function($scope, $stateParams, userStatItem){
         $scope.userStatItem = userStatItem.read({userID: 1}, function(){
             $scope.userStatItem.fullName = $scope.userStatItem.firstName + ' ' + $scope.userStatItem.lastName;
